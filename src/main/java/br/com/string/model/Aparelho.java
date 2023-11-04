26a39be7cd33b7aef49e1cdec6a58d0486a2733c
samuelmelo98 <samuelmelo98@hotmail.com>
2023-11-04 20:05:31 -0300

    Get e Set Entidades.

--- src/main/java/br/com/string/model/Aparelho.java
+++ src/main/java/br/com/string/model/Aparelho.java
@@ -17,5 +17,51 @@ public class Aparelho implements java.io.Serializable {
     @Inject
     private Cliente cliente;
 
+    public Long getIdAparelho() {
+        return idAparelho;
+    }
 
+    public void setIdAparelho(Long idAparelho) {
+        this.idAparelho = idAparelho;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
